4fe49fad34356bc24320cb14b7d80e4c6cb894bc
kosmineOdiseja <aliusbosas@gmail.com>
2021-07-24 11:46:07 +0300

    const let and var

---
+++ VarLetConst/script.js
@@ -0,0 +1,47 @@
+console.log('var =============================')
+
+// var variable 
+// var variable galima pakeisti value, 
+if (true) {
+	var varVariabvle = 'this is treu'
+
+}
+
+var varVariabvle = 'this is false'
+
+console.log(varVariabvle)
+
+
+// var variable galime deklaruoti už funkcijos ar vietos kur mum jos reikia 
+console.log(variablePries)
+var variablePries = 'this is variable'
+console.log(variablePries)
+
+
+console.log('let =============================')
+// Let variable skirtumas 
+if (true) {
+	let letVariable = 'This is true'
+	console.log(letVariable)
+}
+// console.log(letVariable)
+
+let skai = 'true'
+// let skai = 'false'
+
+
+console.log('const =============================')
+
+const constVar = 1
+let letVar = 1
+
+// letVar = 2
+// constVar = 2
+
+
+const constName = {name: 'Alius'}
+
+constName.name = 'Laura'
+console.log(constName)
+
+
