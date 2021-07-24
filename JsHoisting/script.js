b52c59cfb73e33b6fa885949160fc6b5513f3ff9
kosmineOdiseja <aliusbosas@gmail.com>
2021-07-24 17:39:30 +0300

    Js hoisting

---
+++ JsHoisting/script.js
@@ -0,0 +1,17 @@
+// hoisting is the mechaninsm that allows yo to define a function at the bottom of you file yet still use it at the top of the file before its is defined
+//log('hello')
+
+// function log(text){
+// 	console.log(text)
+// }
+
+// const log = (text) => {
+// 	console.log(text)
+// }
+
+console.log(a)
+// undefined
+
+var a = 11
+
+console.log(a)
