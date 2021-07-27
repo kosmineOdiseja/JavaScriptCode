4d6ebc2a19b2c6fd60b2267d384ec19b5ab1ba58
kosmineOdiseja <aliusbosas@gmail.com>
2021-07-27 18:35:27 +0300

    querySelector and queryselectorAll

--- DOMtraversal/script.js
+++ DOMtraversal/script.js
@@ -1,12 +1,14 @@
-const grandparent = document.getElementById("grandparent-id")
+// const grandparent = document.getElementById("grandparent-id")
+// const grandparent = document.querySelector("#grandparent-id")
+//const grandparent = document.querySelector(".grandparent")
 
-
-const parents = Array.from(document.getElementsByClassName("parent"))
-
-
+const parents = document.querySelectorAll(".parent")
+// const parents = Array.from(document.getElementsByClassName("parent"))
 parents.forEach(changeColor)
-
-grandparent.style.backgroundColor = "#333";
+//changeColor(parent)
+// parents.forEach(changeColor)
+// more infor about query selectors: https://www.youtube.com/watch?v=l1mER1bV0N0&ab_channel=WebDevSimplified
+// grandparent.style.backgroundColor = "#333";
 
 function changeColor(element){
 	element.style.backgroundColor = '#888'
