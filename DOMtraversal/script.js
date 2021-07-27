cf2524879400e4ed2518054795f7f6aebec574b3
kosmineOdiseja <aliusbosas@gmail.com>
2021-07-27 18:59:41 +0300

    selecting children

--- DOMtraversal/script.js
+++ DOMtraversal/script.js
@@ -1,15 +1,23 @@
 // const grandparent = document.getElementById("grandparent-id")
-// const grandparent = document.querySelector("#grandparent-id")
-//const grandparent = document.querySelector(".grandparent")
+const grandparent = document.querySelector(".grandparent")
+const parents = Array.from(grandparent.children) 
+const parentOne = parents[1]
+	
+const children = parentOne.children
 
-const parents = document.querySelectorAll(".parent")
+
+changeColor(children[1])
+//parents.forEach(children[0])
+// chnst grandparent = document.querySelector(".grandparent")
+
+// const parent = document.querySelector('parent')
 // const parents = Array.from(document.getElementsByClassName("parent"))
-parents.forEach(changeColor)
-//changeColor(parent)
+
+// changeColor(parent)
 // parents.forEach(changeColor)
 // more infor about query selectors: https://www.youtube.com/watch?v=l1mER1bV0N0&ab_channel=WebDevSimplified
 // grandparent.style.backgroundColor = "#333";
-
+changeColor(grandparent)
 function changeColor(element){
 	element.style.backgroundColor = '#888'
 }
