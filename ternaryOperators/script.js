31de4d9dab343667e32f0a78bc964567869c5612
kosmineOdiseja <aliusbosas@gmail.com>
2021-08-19 17:59:32 +0300

    Ternary Operators

---
+++ ternaryOperators/script.js
@@ -0,0 +1,17 @@
+const number = 50
+let ressult
+
+if  (number >= 50) {
+	result = 'Not to fifty!'
+} else {
+	result = 'It coud ber worse'
+}
+
+console.log(result)
+
+
+number >= 50 ? 'Not to  fifty' : 'It could be worse' 
+rez = number >= 50 ? 'Not to  fifty' : 'It could be worse' // and we can set rez 
+// this is great because we can save a lot of time just writing one line
+console.log(rez)
+
