1c1fa7167d71e9e5ab16fb1632862dcb91a410dd
kosmineOdiseja <aliusbosas@gmail.com>
2021-08-19 11:52:48 +0300

    Null vs Undefined

---
+++ nullVSundefined/script.js
@@ -0,0 +1,12 @@
+// variable not having value 
+console.log(null == undefined)
+// true
+console.log(null === undefined)
+// false
+
+let a = null 
+console.log(a)
+// null 
+
+a = undefined
+console.log(a)
